29b704f275a7c6bca90b97664658b03cdd36daa5
Joey JYH <joeyjyh@gmail.com>
2018-12-27 21:20:35 +0800

    add US part

--- driving.py
+++ driving.py
@@ -2,7 +2,12 @@ country = input('請問你是哪國人? ')
 age = input('請輸入年齡： ')
 age = int(age)
 if country == '台灣':
-	if age>=18:
+	if age >= 18:
+		print('你可以考駕照')
+	else:
+		print('你還無法考駕照')
+elif country == '美國':
+	if age >= 16:
 		print('你可以考駕照')
 	else:
 		print('你還無法考駕照')
